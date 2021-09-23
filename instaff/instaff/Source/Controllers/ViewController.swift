0a11e53de2b0757c6b9a056e4c83d92609f685c9
OnyekachiSamuel <ezeokeonyekachi@gmail.com>
2021-09-23 18:25:32 +0100

    structure app folders and add dependencies for RxSwift and Realm database

---
+++ instaff/instaff/Source/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instaff
+//
+//  Created by Onyekachi Ezeoke on 23/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
