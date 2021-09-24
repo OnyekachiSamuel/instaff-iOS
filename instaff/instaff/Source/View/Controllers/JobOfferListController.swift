79e25de19372aa3daa7467dab40dbd67669df92a
OnyekachiSamuel <ezeokeonyekachi@gmail.com>
2021-09-24 10:37:27 +0100

    rename controller

--- instaff/instaff/Source/View/Controllers/JobOfferListController.swift
+++ instaff/instaff/Source/View/Controllers/JobOfferListController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  JobOfferListController.swift
 //  instaff
 //
 //  Created by Onyekachi Ezeoke on 23/09/2021.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class JobOfferListController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
