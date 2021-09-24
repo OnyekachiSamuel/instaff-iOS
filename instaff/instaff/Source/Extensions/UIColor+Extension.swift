e3036747d2403f1db250439015bb03a8ab30c7c9
OnyekachiSamuel <ezeokeonyekachi@gmail.com>
2021-09-24 16:19:49 +0100

    add extension for `UIColor`

---
+++ instaff/instaff/Source/Extensions/UIColor+Extension.swift
@@ -0,0 +1,36 @@
+//
+//  UIColor+Extension.swift
+//  instaff
+//
+//  Created by Onyekachi Ezeoke on 24/09/2021.
+//
+
+import UIKit
+
+extension UIColor {
+    static let confirmPurple = UIColor(red: 89/255, green: 81/255, blue: 188/255, alpha: 1)
+
+    static let customBlack = UIColor(red: 0, green: 0, blue: 0, alpha: 0.67)
+
+    static let lightPurple = UIColor(red: 115/255, green: 107/255, blue: 225/255, alpha: 1)
+
+    static let lightPink = UIColor(red: 248/255, green: 104/255, blue: 199/255, alpha: 0.1)
+
+    static let lightBlack = UIColor(red: 6/255, green: 6/255, blue: 6/255, alpha: 1)
+
+    static let searchBackgroundColor = UIColor(red: 246/255, green: 246/255, blue: 246/255, alpha: 1)
+
+    static let searchPlaceholderColor = UIColor(red: 140/255, green: 140/255, blue: 140/255, alpha: 1)
+
+    static let ashColor = UIColor(red: 0.69, green: 0.69, blue: 0.69, alpha: 0.5)
+
+    static let faintPurple = UIColor(red: 0.45, green: 0.42, blue: 0.88, alpha: 0.5)
+
+    static let customPurple = UIColor(red: 179/255, green: 62/255, blue: 220/255, alpha: 1)
+
+    static let lightBlue = UIColor(red: 74/255, green: 114/255, blue: 255/255, alpha: 1)
+
+    static let darkBlue = UIColor(red: 74/255, green: 66/255, blue: 194/255, alpha: 1)
+    
+    static let seaBlue = UIColor(red: 9/255, green: 175/255, blue: 211/255, alpha: 1)
+}
